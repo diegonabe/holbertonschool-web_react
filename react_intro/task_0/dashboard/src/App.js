563149eafea2f01853cd8c0c634ea293a49d50d5
Diego <diegonabe@hotmail.com>
2024-03-18 19:08:57 -0500

    ADD Task 0

--- react_intro/task_0/dashboard/src/App.js
+++ react_intro/task_0/dashboard/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
 
 function App() {
@@ -7,16 +7,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>School dashboard</h1> {/* Cambié el texto por "School dashboard" */}
+        <h1>School dashboard</h1>
       </header>
-      <div className="App-body">
-        <p>Login to access the full dashboard</p> {/* Agregué un párrafo con el texto dado */}
-      </div>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+      </body>
       <footer className="App-footer">
-        <p>Copyright 2020 - Holberton School</p> {/* Agregué un párrafo con el texto dado */}
+        <p>Copyright 2020 - Holberton School</p>
       </footer>
     </div>
   );
 }
 
 export default App;
+
